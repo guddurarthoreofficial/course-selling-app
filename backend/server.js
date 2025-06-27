964e6fc553465157a4f445dc7e2077e69ebd842a
Guddu Kumar <guddu748081@gmail.com>
2025-06-27 16:26:37 +0530

    basic setup done for backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,24 @@
-function guddu(){
-    console.log("guddu");
+import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const DB_URI = process.env.MONGO_URI;
+
+//connect to db
+try {
+  await mongoose.connect(DB_URI);
+  console.log("Connected to mongoDB");
+} catch (err) {
+  console.log(err);
 }
 
-guddu();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+
+
+
+app.listen(port, () => console.log(`Server is runnig at port http://localhost:${port}`));
